11be683a16fbb90c0a9e58ac7f44d060cfb950c5
Aibar <aibarchik2019@gmail.com>
2023-01-17 02:08:19 -0800

    Automatic configuration with django-heroku

--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -190,3 +192,6 @@ DJOSER = {
         'user': 'app.serializers.UserSerializer',
     },
 }
+
+
+django_heroku.settings(locals())
